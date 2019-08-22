52e9018bfb01d1b19b3152e6c9eac616c9e522c6
Johanna Reiting <jr087@hdm-stuttgart.de>
2019-08-22 16:10:17 +0200

    added init to gameImpl

--- PeersGameDraw/PeersIntegration/GameDrawHostGameImpl.swift
+++ PeersGameDraw/PeersIntegration/GameDrawHostGameImpl.swift
@@ -12,37 +12,45 @@ import PeersFramework
 class GameDrawHostGameImpl: HostGame {
     var framework: HostFramework?
     
+    var gamePlayers: [GamePlayer] = []
+    
+    weak var containerViewController: UIViewController?
+    
+    init(parentViewController: UIViewController) {
+        containerViewController = parentViewController
+    }
+    
     // Handles messages received from players.
     func handle(message: Data, ofType: String, from peer: Peer) {
-        <#code#>
+        
     }
     
     // Called by the Framework.
     func prepare(players: [Player]) {
-        <#code#>
+        
     }
     
     // Called by the Framework.
     // Showing instruction views for the specified time.
     // Then switching views and starting the drawing timer.
     func play() {
-        <#code#>
+        
     }
     
     // Called by the Framework. Returns the ranked players (winner to looser)
     // after showing the result view.
     func evaluate() -> [GamePlayer] {
-        <#code#>
+        return gamePlayers
     }
     
     // Forces the game to stop, even mid-lifecycle.
     func terminate(completionHandler: @escaping () -> Void) {
-        <#code#>
+        
     }
     
     // Handle a player exiting during the game.
     func lostPlayer(_ player: Player) {
-        <#code#>
+        
     }
     
 }
--- PeersGameDraw/PeersIntegration/GameDrawPlayerGameImpl.swift
+++ PeersGameDraw/PeersIntegration/GameDrawPlayerGameImpl.swift
@@ -32,19 +32,19 @@ class GameDrawPlayerGameImpl: PlayerGame {
     }
     
     func handle(message: Data, ofType: String) {
-        <#code#>
+        
     }
     
     // Called by the framework.
     // Show the instructions view to take (partial) view control from the Peers App.
     func play() {
-        <#code#>
+        
     }
     
     // Called by the framework.
     // Remove the game view to pass back view control to the Peers App.
     func terminate(completionHandler: @escaping () -> Void) {
-        <#code#>
+        
     }
     
     
